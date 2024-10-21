29cc0a481ef1beca8261bd9fd62dcebf32f5677a
El Mehdi <azghoulmehdi899@gmail.com>
2024-10-21 22:28:40 +0100

    Update auth middleware for token expiration handling

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,17 +1,21 @@
-const jwt = require('jsonwebtoken');
-
-// decode token
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-        req.body.userId = decodedToken.userId;
-        next();
-    } catch (error) {
-        res.send ({
-            success: false,
-            message: error.message,
-        })
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    req.body.userId = decodedToken.userId;
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({
+        success: false,
+        message: "jwt expired",
+      });
     }
+    res.status(401).json({
+      success: false,
+      message: "Authentication failed",
+    });
+  }
 };
