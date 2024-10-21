f2138c75e970dd5b8c1ef1eafab7ea9d24d41530
El Mehdi <azghoulmehdi899@gmail.com>
2024-10-21 22:17:35 +0100

    Update user authentication API calls

--- client/src/apiCalls/users.js
+++ client/src/apiCalls/users.js
@@ -1,36 +1,37 @@
-const { axiosInstance } = require('.');
+const { axiosInstance } = require(".");
 
 // user login
-
 export const loginUser = async (payload) => {
-    try {
-        const { data } = await axiosInstance.post('/api/users/login', payload);
-        return data;
-        
-    } catch (error) {
-        return error.response.data;
-    }};
+  try {
+    const { data } = await axiosInstance.post("/api/users/login", payload);
+    if (data.success) {
+      localStorage.setItem("token", data.data);
+    }
+    return data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
 
 // user registration
 
 export const registerUser = async (payload) => {
-    try {
-        const { data } = await axiosInstance.post('/api/users/register', payload);
-        return data;
-        
-    } catch (error) {
-        return error.response.data;
-    }}
+  try {
+    const { data } = await axiosInstance.post("/api/users/register", payload);
+    return data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
 
 // get user information
 
 export const getUserInfo = async () => {
-    try {
-        const { data } = await axiosInstance.post('/api/users/get-user-info', );
-            
-        return data;
-        
-    } catch (error) {
-        return error.response.data;
-    }
-    }
+  try {
+    const { data } = await axiosInstance.post("/api/users/get-user-info");
+
+    return data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
