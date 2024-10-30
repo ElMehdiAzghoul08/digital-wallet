d5ed5ba433b7d7b2b88466a8e7814b8ab54877d9
El Mehdi <azghoulmehdi899@gmail.com>
2024-10-30 07:29:19 +0100

    feat: Implement server functionality
    - Set up Express server for API endpoints.
    - Added middleware for parsing JSON and handling CORS.
    - Defined routes for user authentication and transaction management.

--- server/server.js
+++ server/server.js
@@ -6,11 +6,14 @@ const dbConfig = require("./config/dbConfig");
 const usersRoute = require("./routes/usersRoute");
 const transactionsRoute = require("./routes/TransactionsRoute");
 
+const requestsRoute = require("./routes/requestsRoute");
+
 require("dotenv").config();
 app.use(express.json());
 
 app.use("/api/users", usersRoute);
 app.use("/api/transactions", transactionsRoute);
+app.use("/api/requests", requestsRoute);
 
 const PORT = process.env.PORT || 5000;
 
